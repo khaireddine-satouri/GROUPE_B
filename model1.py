1ac28cd272f7b94875d1cb7d90c964586e5eaba0
Youcef Anis <dahlouk_anis@hotmail.com>
2023-03-17 18:54:32 +0100

    Entrainement Modèle 4

--- model1.py
+++ model1.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 
 # Define model architecture
 model = tf.keras.Sequential([
-    tf.keras.layers.Dense(10, activation='relu', input_shape=(2,)),
+    tf.keras.layers.Dense(10, activation='relu', input_shape=(5,)),
     tf.keras.layers.Dense(1)
 ])
 
@@ -10,14 +10,14 @@ model = tf.keras.Sequential([
 model.compile(loss='mse', optimizer='adam')
 
 # Train model (example)
-x_train = [[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]]
+x_train = [[1, 2, 3, 4, 5], [3, 4, 5, 7, 9], [5, 6, 4, 1, 0], [7, 8, 9, 10, 11], [9, 10, 12, 4, 8]]
 y_train = [5, 7, 9, 11, 13]
 model.fit(x_train, y_train, epochs=100)
 
 # Predict using trained model
-x_test = [[11, 12], [13, 14], [15, 16]]
+x_test = [[11, 12, 21, 32, 5], [13, 14, 12, 5, 4], [15, 16, 12, 10, 12],  [7, 8, 9, 10, 11], [9, 10, 12, 4, 8]]
 y_pred = model.predict(x_test)
 print("Predictions: \n", y_pred)
 
 # Save model to file
-model.save('model1')
+model.save('model4')
