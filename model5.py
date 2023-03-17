cfc4e2d1f02825603055ae1d3cc8b597c45e7d80
khaireddine satouri <khaireddine.satouri@gmail.com>
2023-03-17 18:44:58 +0100

    Entrainement Modèle 5

--- model5.py
+++ model5.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 
 # Define model architecture
 model = tf.keras.Sequential([
-    tf.keras.layers.Dense(10, activation='relu', input_shape=(2,)),
+    tf.keras.layers.Dense(10, activation='relu', input_shape=(6,)),
     tf.keras.layers.Dense(1)
 ])
 
@@ -10,14 +10,14 @@ model = tf.keras.Sequential([
 model.compile(loss='mse', optimizer='adam')
 
 # Train model (example)
-x_train = [[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]]
+x_train = [[1, 2 , 3 , 4 , 6 ,7], [1, 2 , 3 , 4 , 6 ,7], [1, 2 , 3 , 4 , 6 ,7], [1, 2 , 3 , 4 , 6 ,7], [1, 2 , 3 , 4 , 6 ,7]]
 y_train = [5, 7, 9, 11, 13]
 model.fit(x_train, y_train, epochs=100)
 
 # Predict using trained model
-x_test = [[11, 12], [13, 14], [15, 16]]
+x_test = [[11, 12 , 13 , 14 , 16 ,17], [11, 14 , 16 , 14 , 16 ,18], [19, 20 , 21 , 22 , 23 ,24]]
 y_pred = model.predict(x_test)
 print("Predictions: \n", y_pred)
 
 # Save model to file
-model.save('model1')
+model.save('model5')
