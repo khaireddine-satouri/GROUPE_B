8a42d864eb4d3f8817fef680c9036ea9d21fe603
Youcef Anis <dahlouk_anis@hotmail.com>
2023-03-17 19:06:19 +0100

    Entrainement Modèle 2

--- model2.py
+++ model2.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 
 # Define model architecture
 model = tf.keras.Sequential([
-    tf.keras.layers.Dense(10, activation='relu', input_shape=(2,)),
+    tf.keras.layers.Dense(10, activation='relu', input_shape=(3,)),
     tf.keras.layers.Dense(1)
 ])
 
@@ -10,14 +10,14 @@ model = tf.keras.Sequential([
 model.compile(loss='mse', optimizer='adam')
 
 # Train model (example)
-x_train = [[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]]
+x_train = [[88, 17, 15], [13, 14, 1800], [1777, 178455 , 65], [7, 815,1], [7709, 4510, 456]]
 y_train = [5, 7, 9, 11, 13]
 model.fit(x_train, y_train, epochs=100)
 
 # Predict using trained model
-x_test = [[11, 12], [13, 14], [15, 16]]
+x_test = [[11, 12, 487], [13, 14, 44], [15, 16, 1744]]
 y_pred = model.predict(x_test)
 print("Predictions: \n", y_pred)
 
 # Save model to file
-model.save('model1')
+model.save('model2')
